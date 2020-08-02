18e5f136c429c90b90f05b1be8bc78fe04fa7111
Rahul Patil <rahulcs629@gmail.com>
2020-08-02 16:17:54 +0530

    Concatenation tuple

---
+++ Concatenaion_tuple.py
@@ -0,0 +1,16 @@
+#Concatenation of tuple
+tuple1 = (0,1,2,3)
+tuple2 = ("Rahul","Patil")
+tuple3 = tuple1 + tuple2
+
+#Printing first tuple
+print("\nprint first tuple")
+print(tuple1)
+
+#printing secound tuple
+print("\nprint secound tuple")
+print(tuple2)
+
+#printing final tuple
+print("\nprint final tuple")
+print(tuple3)
